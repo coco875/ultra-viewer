00b496ba77bed3023a35662edbde05a238ce89f4
coco875 <pereira.jannin@gmail.com>
2024-12-09 16:15:04 +0100

    small test

--- src/modloader/Wasm.cpp
+++ src/modloader/Wasm.cpp
@@ -1,8 +1,10 @@
-// #include <cstdint>
-// #include <string>
-// #include <iostream>
-// #include <vector>
-// #include <filesystem>
+#include <cstdint>
+#include <string>
+#include <iostream>
+#include <vector>
+#include <filesystem>
+#include <fstream>
+#include <wasmtime.hh>
 // #include <wasm_export.h>
 // #include <wasm_exec_env.h>
 
@@ -103,7 +105,7 @@
 
 // static char global_heap_buf[512 * 1024];
 
-void load_mods() {
+// void load_mods() {
 //     wasm_runtime_init();
 
 //     RuntimeInitArgs init_args;
@@ -133,4 +135,61 @@ void load_mods() {
 //         // Report the error:
 //         std::cout << "CreateDirectoryRecursive FAILED, err: " << err.message() << std::endl;
 //     }
+// }
+
+using namespace wasmtime;
+
+std::string readFile(const char *name) {
+  std::ifstream watFile;
+  watFile.open(name);
+  std::stringstream strStream;
+  strStream << watFile.rdbuf();
+  return strStream.str();
+}
+
+std::vector<uint8_t> readBin(const char *name){
+    std::ifstream instream(name, std::ios::in | std::ios::binary);
+    std::vector<uint8_t> data((std::istreambuf_iterator<char>(instream)), std::istreambuf_iterator<char>());
+    return data;
+}
+
+void load_mods() {
+  // First the wasm module needs to be compiled. This is done with a global
+  // "compilation environment" within an `Engine`. Note that engines can be
+  // further configured through `Config` if desired instead of using the
+  // default like this is here.
+  std::cout << "Compiling module\n";
+  Engine engine;
+  auto module =
+      Module::compile(engine, readFile("examples/hello.wat")).unwrap();
+
+  // After a module is compiled we create a `Store` which will contain
+  // instantiated modules and other items like host functions. A Store
+  // contains an arbitrary piece of host information, and we use `MyState`
+  // here.
+  std::cout << "Initializing...\n";
+  Store store(engine);
+
+  // Our wasm module we'll be instantiating requires one imported function.
+  // the function takes no parameters and returns no results. We create a host
+  // implementation of that function here.
+  std::cout << "Creating callback...\n";
+  Func host_func =
+      Func::wrap(store, []() { std::cout << "Calling back...\n"; });
+
+  // Once we've got that all set up we can then move to the instantiation
+  // phase, pairing together a compiled module as well as a set of imports.
+  // Note that this is where the wasm `start` function, if any, would run.
+  std::cout << "Instantiating module...\n";
+  auto instance = Instance::create(store, module, {host_func}).unwrap();
+
+  // Next we poke around a bit to extract the `run` function from the module.
+  std::cout << "Extracting export...\n";
+  auto run = std::get<Func>(*instance.get(store, "run"));
+
+  // And last but not least we can call it!
+  std::cout << "Calling export...\n";
+  run.call(store, {}).unwrap();
+
+  std::cout << "Done\n";
 }
