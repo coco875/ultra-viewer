8f8bc6b15dd22d1420017965c4bdf0e8cd914d7d
coco875 <59367621+coco875@users.noreply.github.com>
2025-02-14 19:12:56 +0100

    bump lus

--- src/port/ui/ImguiUI.cpp
+++ src/port/ui/ImguiUI.cpp
@@ -196,7 +196,6 @@ void DrawSettingsMenu(){
             { Ship::WindowBackend::FAST3D_DXGI_DX11, "DirectX" },
             { Ship::WindowBackend::FAST3D_SDL_OPENGL, "OpenGL"},
             { Ship::WindowBackend::FAST3D_SDL_METAL, "Metal" },
-            { Ship::WindowBackend::FAST3D_SDL_BGFX, "BGFX" },
         };
 
         ImGui::Text("Renderer API (Needs reload)");
